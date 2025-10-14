3f5955fd64f9e29f2c06bbd76f9c33b93f1fbf95
Nicolas Juniar Kurniawan <nicolas@investree.id>
2025-10-14 14:20:59 +0700

    [nico] add github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// Ganti dengan nama repository GitHub kamu
+// misal repo-nya: github.com/nicolasjuniar/nicolasjuniar.github.io
+// maka `base` harus "/"
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/",
+});
